c18bbbd102ce8cf427e931ec946c5ec31851b53b
aaaka5h <akash7adhikary@gmail.com>
2025-03-07 15:08:56 -0800

    boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # random-shuffle-comparison
-Runtime comparisons for various shuffling algorithms
+
+Runtime comparisons for various shuffling algorithms applied to $[n]$ for $n \in \{10^4, 10^6, 10^8\}$
---
+++ random-shuffle/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Shuffling Algorithms Runtime Comparison</h1>
+            <div className="App-content">
+                <div>
+                    <h2>BallsBinsShuffle</h2>
+                </div>
+                <div>
+                    <h2>BinaryShuffle</h2>
+                </div>
+                <div>
+                    <h2>The one from class</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
